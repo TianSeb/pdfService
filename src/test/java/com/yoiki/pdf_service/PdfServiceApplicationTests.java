1d35ab28a1aa184b7c12811aa8ae4c2f5d6d23a1
TianSeb <sebastianalasia86@gmail.com>
2025-02-11 02:29:06 -0300

    initial commit

---
+++ src/test/java/com/yoiki/pdf_service/PdfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoiki.pdf_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
